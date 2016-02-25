6579f7f42db42e55cb3bec4ba6190149a8d9719b
michmauzer004 <michmauzer004@gmail.com>
2016-02-25 23:09:56 +0200

    jetty sample project for teamcity

---
+++ src/main/java/com/packtpub/Calculator.java
@@ -0,0 +1,6 @@
+package com.packtpub;
+public class Calculator {
+    public int sum(int x, int y) {
+        return x + y;
+    }
+}
---
+++ src/test/java/com/packtpub/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.packtpub;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+/**
+ * Created by webstddesign on 21.02.2016.
+ */
+public class CalculatorTest extends TestCase {
+
+    public void testSum() throws Exception {
+        Calculator calculator = new Calculator();
+        int sum = calculator.sum(1, 2);
+        Assert.assertEquals(3, sum);
+    }
+}

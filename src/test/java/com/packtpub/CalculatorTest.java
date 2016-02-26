331f91b3648b4b99e2bb59fb251f11e50e2f0fbe
michmauzer004 <michmauzer004@gmail.com>
2016-02-26 23:57:41 +0200

    added out.println line

--- src/test/java/com/packtpub/CalculatorTest.java
+++ src/test/java/com/packtpub/CalculatorTest.java
@@ -12,8 +12,8 @@ public class CalculatorTest extends TestCase {
         int sum = calculator.sum(1, 2);
         Assert.assertEquals(3, sum);
         /*comment*/
-        public void testBad() throws Exception {
-            Assert.fail("Some bad test has failed");
+        public void testGood() throws Exception {
+            Assert.assertTrue("Some bad test has failed",true);
         }
     }
 }
